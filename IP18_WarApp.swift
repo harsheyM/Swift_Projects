10cb534d871b71d8a606670d39e5d69b60468716
harsheyM <harshithamahesh421@gmail.com>
2026-01-13 16:33:06 -0600

    Add files via upload

---
+++ IP18_WarApp.swift
@@ -0,0 +1,17 @@
+//
+//  IP18_WarApp.swift
+//  IP18-War
+//
+//  Created by Mahesh, Harshitha on 4/10/25.
+//
+
+import SwiftUI
+
+@main
+struct IP18_WarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
